f383a5fde03069b5f93053396328ad17921d4468
Manh Pham <phamducmanh1989@gmail.com>
2020-12-07 14:53:26 +0700

    support IB

--- Sources/ALRadioButtons/Views/ALRadioGroup.swift
+++ Sources/ALRadioButtons/Views/ALRadioGroup.swift
@@ -274,7 +274,7 @@ public final class ALRadioGroup: UIControl {
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
 }
 
